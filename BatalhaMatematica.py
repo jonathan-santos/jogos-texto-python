30092c8f4c7cd3d4f6ae4fd63fa9fb84273fa9d9
GuilhermeFrancoGrillo <96924733+GuilhermeFrancoGrillo@users.noreply.github.com>
2022-01-01 10:36:51 -0300

    Update BatalhaMatematica.py

--- BatalhaMatematica.py
+++ BatalhaMatematica.py
@@ -5,36 +5,46 @@ vidaJaque = 3
 vezJaque = True
 
 print("Bem vindo as aventuras matemáticas de Leptude o Mago de patinete")
-print("Leptude por algum motivo entra em batalha com o sapo mago professor de matemática interdimensional Jaque, para vencer dele você precisa acertar suas perguntas de matemática ou então ele vai te matar de tédio")
+print(
+    "Leptude por algum motivo entra em batalha com o sapo mago professor de matemática interdimensional Jaque, para vencer dele você precisa acertar suas perguntas de matemática ou então ele vai te matar de tédio")
 
-while(vidaLeptude > 0 and vidaJaque > 0):
-    if(vezJaque):
-        perguntaJaque = str(random.randint(1, 25))+"+"+str(random.randint(1,25))
-        resposta = input("Jaque te ataca com a pergunta: quanto é "+perguntaJaque+"? ")
-        print("A resposta da pergunta era",eval(perguntaJaque))
-        if int(resposta) == eval(perguntaJaque):
+while vidaLeptude > 0 and vidaJaque > 0:
+    if vezJaque:
+        perguntaJaque = str(random.randint(1, 25)) + "+" + str(random.randint(1, 25))
+        con = 1
+        while con:
+            try:
+                resposta = int(input("Jaque te ataca com a pergunta: quanto é " + perguntaJaque + "? "))
+            except:
+                print("escreva certo para o jaque enteder você")
+            else:
+                con -= 1
+        print("A resposta da pergunta era", eval(perguntaJaque))
+        if resposta == eval(perguntaJaque):
             print("Acertou mizerávi")
         else:
-            vidaLeptude-=1
-            print("Errou mizerávi, agora você está com",vidaLeptude,"pontos de vida")
+            vidaLeptude -= 1
+            print("Errou mizerávi, agora você está com", vidaLeptude, "pontos de vida")
     else:
-        resposta = eval(input("Agora Leptude, ataque jaque com uma conta de matemática: "))
-        
+        a = str(input("Agora Leptude, ataque jaque com uma conta de matemática: ")).split()
+        resposta = eval(''.join(a))
+
         respostaJaque = random.randint(resposta - 1, resposta + 1)
 
-        print("A resposta de sua pergunta é",resposta,"e Jaque respondeu ",respostaJaque)
+        print("A resposta de sua pergunta é", resposta, "e Jaque respondeu ", respostaJaque)
         if resposta == respostaJaque:
-           print("Jaque acertou sua pergunta")
+            print("Jaque acertou sua pergunta")
         else:
-            vidaJaque-=1
-            print("Jaque errou sua pergunta. Agora Jaque está com",vidaJaque,"pontos de vida")
+            vidaJaque -= 1
+            print("Jaque errou sua pergunta. Agora Jaque está com", vidaJaque, "pontos de vida")
 
     print("")
-    vezJaque = not(vezJaque)
+    vezJaque = not vezJaque
 
-if(vidaJaque > 0):
+if (vidaJaque > 0):
     print("Você perdeu a batalha e agora Jaque começa a te ensinar matemática, até que você morre de tédio")
 else:
-    print("Jaque perdeu a batalha, então você começa a zuar ele, ele fica nervoso e volta a sua dimensão para ter reforço de matemática")
+    print(
+        "Jaque perdeu a batalha, então você começa a zuar ele, ele fica nervoso e volta a sua dimensão para ter reforço de matemática")
 
 print("FIM")
